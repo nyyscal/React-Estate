bfe255880c63bc32f738df157de6dc1c9127b65e
Nischal <nyyscal541556@gmail.com>
2024-09-27 16:40:02 +0545

    CRUD

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser"
 import authRouter from "./routes/auth.route.js";
 import postRouter from "./routes/post.route.js";
 import testRouter from "./routes/test.routes.js";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -16,8 +17,9 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-app.use("/api/posts", postRouter)
 app.use("/api/auth", authRouter)
+app.use("/api/users", userRouter)
+app.use("/api/posts", postRouter)
 app.use("/api/test", testRouter)
 
 
