8b2011a8083fb1333337a83448315dba655e4e9a
Nischal <nyyscal541556@gmail.com>
2024-09-27 15:51:56 +0545

    Layout

---
+++ api/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from 'dotenv';
+import cors from "cors";
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.route.js";
+import postRouter from "./routes/post.route.js";
+import testRouter from "./routes/test.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({origin: process.env.CLIENT_URL, credentials:true}))
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use("/api/posts", postRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/test", testRouter)
+
+
+app.listen(8800, ()=>{
+  console.log("Server is Activated!")
+})
---
+++ api/routes/post.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router()
+
+router.get("/test",(req,res)=>{
+  console.log("Router Works!")
+})
+router.post("/test",(req,res)=>{
+  console.log("Router Works!")
+})
+router.put("/test",(req,res)=>{
+  console.log("Router Works!")
+})
+router.delete("/test",(req,res)=>{
+  console.log("Router Works!")
+})
+
+export default router
