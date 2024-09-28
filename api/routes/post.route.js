07c5890784d30a97e4f91ef8ed423cf9de561261
Nischal <nyyscal541556@gmail.com>
2024-09-28 12:43:33 +0545

    Post Model

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,18 +1,13 @@
 import express from "express";
+import {verifyToken} from "../middleware/verifyToken.js"
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
 
 const router = express.Router()
 
-router.get("/test",(req,res)=>{
-  console.log("Router Works!")
-})
-router.post("/test",(req,res)=>{
-  console.log("Router Works!")
-})
-router.put("/test",(req,res)=>{
-  console.log("Router Works!")
-})
-router.delete("/test",(req,res)=>{
-  console.log("Router Works!")
-})
+router.get("/",getPosts)
+router.get("/:id",getPost)
+router.post("/",verifyToken, addPost)
+router.put("/:id",verifyToken, updatePost)
+router.delete("/:id",verifyToken,deletePost)
 
 export default router
